f9c8948109137a7dfb73f0ed9513ebbc7ef728f0
RogerSi666 <107162778+RogerSi666@users.noreply.github.com>
2022-09-08 16:46:04 +0900

    商品出品機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
-    @items = Item.all
   end
 
   def new
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -41,6 +41,12 @@ RSpec.describe Item, type: :model do
     end
 
     context '出品ができないとき' do
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
       it '１枚画像がないと出品できない' do
         @item.image = nil
         @item.valid?

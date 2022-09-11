3f29040efdc544f5cf010c691fd6270f1416ef2a
RogerSi666 <107162778+RogerSi666@users.noreply.github.com>
2022-09-11 15:54:16 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
- 
   devise_for :users
 
-  root  'items#index'
+  root to: "items#index"
 
-  resources :items, only: [:index, :new, :create]
-
-  
+  resources :items
   end

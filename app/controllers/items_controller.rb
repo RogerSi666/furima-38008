0c671676ac28e0d5f1c6548b2925143b8a0c753b
RogerSi666 <107162778+RogerSi666@users.noreply.github.com>
2022-09-13 18:02:19 +0900

    商品削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,8 +24,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.user_id == current_user.id
     @item.destroy
     redirect_to root_path
+  else
+    redirect_to root_path
+  end
   end
 
   def edit
